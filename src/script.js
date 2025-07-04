59946579fe7a4d39b7bdef5becde6a8b1c99804b
naveen <naveen.prakash.kv@gmail.com>
2025-07-04 12:04:52 +0530

    .gitignore added

---
+++ src/script.js
@@ -0,0 +1,55 @@
+import * as THREE from "three";
+
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// initialize the scene
+const scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+// add objects to the scene
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
+
+// initialize the camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  30
+);
+camera.position.z = 3;
+
+// initialize the renderer
+const canvas = document.querySelector("canvas.threejs");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+//orbit control
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.update();
+console.log(window.devicePixelRatio);
+
+// resize event listener
+window.addEventListener("resize", () => {
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// animation
+function animation() {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animation);
+}
+animation();
