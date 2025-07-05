49e071b06b6cac363c83f44ad9f7ba0fb86a5503
naveen <naveen.prakash.kv@gmail.com>
2025-07-05 16:14:51 +0530

    Refactor cube and plane creation: update materials, remove unused code, and fix rotation logic

--- src/script.js
+++ src/script.js
@@ -7,40 +7,30 @@ const scene = new THREE.Scene();
 // Adding axes helper
 const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
-// creating the group
-// const group = new THREE.Group();
-
-// add objects to the scene
-// buffer geometry
-// const vertex = new Float32Array([0, 0, 0, 0, 2, 0, 2, 0, 0]);
-// const bufferAttribute = new THREE.BufferAttribute(vertex, 3);
-// const geometry = new THREE.BufferGeometry();
-// geometry.setAttribute("position", bufferAttribute);
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 
-const cubeMaterial = new THREE.MeshBasicMaterial({
-  color: "red",
-});
+const cubeMaterial = new THREE.MeshLambertMaterial();
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 cubeMaterial.transparent = true;
 cubeMaterial.opacity = 0.5;
-// creating second object
 
+// creating second object
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
-const planeMaterial = new THREE.MeshBasicMaterial({
-  color: "blue",
+const planeMaterial = new THREE.MeshLambertMaterial({
   side: THREE.DoubleSide,
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
 planeMesh.position.z = 2;
 
-// scaling size of the cube
-// cubeMesh.scale.set(3, 3, 1);
-
-// creating more curbe
-scene.add(cubeMesh);
+// Adding light source to  the scene
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientlight);
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(2, 2, 2);
+scene.add(pointLight);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -76,10 +66,12 @@ window.addEventListener("resize", () => {
 });
 
 function animation() {
-  planeGeometry.rotateZ(0.01);
-  cubeGeometry.rotateX(0.01);
-  controls.update();
+  planeMesh.rotation.z += 0.01; // Rotate the mesh, not geometry
+  cubeMesh.rotation.x += 0.01; // Same here
+
+  controls.update(); // For damping effect
   renderer.render(scene, camera);
   window.requestAnimationFrame(animation);
 }
+
 animation();
